3e67ae3b258f768051bab252d42fb0c033ca2f2d
mthang1801 <mthang1801@gmail.com>
2020-04-28 06:59:06 +0700

    v[1.0.0] add foodback at footer

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -15,6 +15,7 @@ const Footer = () => {
       <div className={styles.right_side}>
         <p className={styles.social_fb}>Follow me at: <a href="https://www.facebook.com/maivanthang95" target="_blank"><i className="fab fa-facebook-square fa-lg" aria-hidden="true"></i></a></p>
         <p className={styles.social_github}>Visit my projects at: <a href="https://github.com/mthang1801"><i className="fab fa-github-square fa-lg"></i></a> </p>
+        <p className={styles.send_mail}>Mọi ý kiến đóng góp vui lòng gửi <a href="mailto:mthang1801@gmail.com?subject=Feedback&body=" target="popup" onClick="window.open('http://kanishkkunal.in','popup','width=600,height=600');return false;">tại đây</a></p>
       </div>
     </div>
   )
